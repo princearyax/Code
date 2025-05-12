de0827346c47ec41c28b4c716e5529bc34d9070d
Prince Arya <prince422005@gmail.com>
2025-05-12 08:00:09 +0530

    eroor handling alone

---
+++ 4errorHandle/Error_Basics/my.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+// app.use(morgan('tiny'));
+
+// app.use((req, res, next) => {
+//     req.requestTime = Date.now();
+//     console.log(req.method, req.path);
+//     next();
+// })
+
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
+})
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    console.log(req.query);
+    if (password === "arya") {
+        B.ok();
+        next();
+    }else 
+    res.send("YOU NEED A PASSWORD!");
+}
+
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!")
+//     return next();
+//     console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE!!!")
+//     return next();
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY THIRD MIDDLEWARE!!!")
+//     return next();
+// })
+
+
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('HOME PAGE!')
+})
+
+app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('WOOF WOOF!')
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
+})
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
+})
+
+app.use((err, req, res, next) => {
+    console.log("******************************************");
+    console.log("*****************ERROR*****************");
+    console.log("******************************************");
+    console.log(err);
+    next();
+    // next(err)
+})
+
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000')
+})
