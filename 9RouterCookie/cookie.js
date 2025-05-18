296e05ca5aeb99848c08c3657badf8a2cdef7096
Prince Arya <prince422005@gmail.com>
2025-05-18 16:41:49 +0530

    cookie ongoing

--- 9RouterCookie/cookie.js
+++ 9RouterCookie/cookie.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());  // to parse incoming cookies
 
 app.get("/", (req, res)=>{
     res.send("hi");
-})
+    console.log(req.cookies);
+});
 
 app.get("/cookie", (req, res)=>{
     res.cookie("name", "arya"); //sending this cookie to the client
+    res.cookie("age","20");
     res.send("cookie set"); 
-})
+});
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
-})
+});
