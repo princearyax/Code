682057fc439bb2d175bbc54bf8d22637e9d5b3e4
Prince Arya <prince422005@gmail.com>
2025-05-18 17:10:04 +0530

    done signing

--- 9RouterCookie/cookie.js
+++ 9RouterCookie/cookie.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cookieParser = require("cookie-parser");
-app.use(cookieParser());  // to parse incoming cookies
+app.use(cookieParser("aryax"));  // to parse incoming cookies //that string will be used as secret to sign and erify
 
 app.get("/", (req, res)=>{
     res.send("hi");
@@ -13,6 +13,17 @@ app.get("/cookie", (req, res)=>{
     res.cookie("name", "arya"); //sending this cookie to the client
     res.cookie("age","20");
     res.send("cookie set"); 
+});
+
+app.get("/getsignedcookie", (req, res)=>{
+    res.cookie("name", "arya", {signed: true}); // sending this cookie to the
+    res.send("sending signed cookie");
+})
+
+app.get("/verifycookie", (req, res)=>{
+    // res.send(req.cookies); 
+    res.send(req.signedCookies);
+
 });
 
 app.listen(port, ()=>{
