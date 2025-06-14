3e6e754b6259e816c0b8949b0e4a4f0694d23333
Prince Arya <prince422005@gmail.com>
2025-06-14 11:19:08 +0530

    finishing

--- CampGround/index.js
+++ CampGround/index.js
@@ -15,6 +15,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const authStrategy = require("passport-local");
 const User = require("./models/user");
+const helmet = require("helmet");
 
 const sanitizeV5 = require('./utilities/mongoSanitizeV5.js');//security
 // const mongoSanitize = require('express-mongo-sanitize');
@@ -67,6 +68,58 @@ const sessionConfig  = {
 app.use(session(sessionConfig));
 //will be able to flash something with req.flash by passing in a key and a value
 app.use(flash());
+app.use(helmet({contentSecurityPolicy: false}));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    // "https://api.tiles.mapbox.com/",
+    // "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    // "https://api.mapbox.com/",
+    // "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const connectSrcUrls = [
+    // "https://api.mapbox.com/",
+    // "https://a.tiles.mapbox.com/",
+    // "https://b.tiles.mapbox.com/",
+    // "https://events.mapbox.com/",
+    "https://api.maptiler.com/", // add this
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dnmguhtpt/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com",
+                "https://api.maptiler.com/"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 
 app.use(passport.initialize()); //must be used after app.use(session(...))
 app.use(passport.session());
